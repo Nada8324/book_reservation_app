0bf1ea19526a8daf93338552946f2da7fce89bc9
Nada8324 <mohsennada874@gmail.com>
2025-12-18 03:38:31 +0200

    create splash view body

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:book_reservation_app/features/splash/presentation/views/splash_view.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(home: SplashView());
+  }
+}
